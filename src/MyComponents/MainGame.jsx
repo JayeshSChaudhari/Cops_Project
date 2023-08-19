0737afe805b61fa21ec2f88b050b1720b1b51bb0
JayeshSChaudhari <chaudhari.jayesh.cd.mec22@itbhu.ac.in>
2023-08-19 19:19:24 +0530

    adding markup and style

--- src/MyComponents/MainGame.jsx
+++ src/MyComponents/MainGame.jsx
@@ -14,37 +14,35 @@ export default function MainGame() {
     return (
 
         <>
-            <div id="Screen">
-
-                <p>Choose among - Rock , Paper & Scissor</p>
 
 
-
-                <div className="GameBox">
-                    <div id="displayCompAndUser">
-                        <div>Played By Computer</div>
-                        <div>Played by User</div>
-                        <div id="CompPlayed">L</div>
-                        <div id="UserPlayed">L</div>
-
-                    </div>
-
-
-
-                    <div id="ChooseOne">
-
-                    </div>
-
-
-
-
-
-
-                </div>
-
-
-            </div>
-
+    <div className="container">
+        <div className="scores">
+            <p>Computer : 
+                <span id="computer_score">0</span>
+            </p>
+            <p>
+                You :
+                <span id="user_score">0</span>
+            </p>
+        </div>
+        <div className="weapons">
+            <button onclick="checker('rock')">
+                <i className="far fa-hand-rock"></i>
+            </button>
+            <button onclick="checker('paper')">
+                <i className="far fa-hand-paper"></i>
+            </button>
+            <button onclick="checker('scissor')">
+                <i className="far fa-hand-scissors"></i>
+            </button>
+        </div>
+        <div className="details">
+            <p id="user_choice"></p>
+            <p id="comp_choice"></p>
+            <p id="result"></p>
+        </div>
+    </div>
         </>
 
     )
