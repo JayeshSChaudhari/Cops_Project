65c4dfd0a6c0be84e676ca5ca5f9ec84396a9d63
JayeshSChaudhari <chaudhari.jayesh.cd.mec22@itbhu.ac.in>
2023-08-19 20:16:24 +0530

    time over submit

--- src/MyComponents/MainGame.jsx
+++ src/MyComponents/MainGame.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../AllCSS/MainGame.css'
-import {useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 
 
@@ -8,27 +8,71 @@ import {useState , useEffect} from 'react'
 
 
 export default function MainGame() {
+    const [compChoose, setCompChoose] = useState("");
+    const [userChose, setUserChoose] = useState(null);
+
+    const [result, setResult] = useState(() => {
+
+        if (((compChoose === 1) && (userChose === 2)) || ((compChoose === 2) && (userChose === 3)) || ((compChoose === 3) && (userChose === 1))) {
+            //user wins
+            setResult("User Won");
+            
+        }
+        
+        else if (((compChoose === 2) && (userChose === 1)) || ((compChoose === 3) && (userChose === 2)) || ((compChoose === 1) && (userChose === 3))) {
+            //comp wins
+            setResult("Computer Won");
+        }
+        else {
+            //tie
+            setResult("Tie");
+        }
+    }
+    )
 
     useEffect(() => {
-        const randomID = Math.floor(Math.random() * 4);
+        const randomID = Math.floor(1 + Math.random() * 4);
         console.log(randomID);
+
         setCompChoose(randomID);
-    },[]);
+        console.log(compChoose);
+    }, []);
 
 
 
-    const [compChoose , setCompChoose]  = useState(null);
 
-    
+    function choseRock() {
+        setUserChoose(1);
+        console.log(userChose);
 
-    
-        
-       
+    }
+    function chosePaper() {
+        setUserChoose(2);
+        console.log(userChose);
 
+    }
+    function choseScissor() {
+        setUserChoose(3);
+        console.log(userChose);
+
+    }
+
+
+
+    /*
     
+    1 rock
+    2 paper
+    3 scissor
     
-    
-    
+     */
+
+
+
+
+
+
+
 
 
 
@@ -64,11 +108,11 @@ export default function MainGame() {
                     </p>
                 </div>
                 <div className="weapons">
-                    <button ><p>Rock</p>
+                    <button id='choseRock' onClick={() => choseRock()}><p>Rock</p>
                     </button>
-                    <button ><p>Paper</p>
+                    <button id='chosePaper' onClick={() => chosePaper()}><p>Paper</p>
                     </button>
-                    <button ><p>Scissor</p>
+                    <button id='choseScissor' onClick={() => choseScissor()}><p>Scissor</p>
                     </button>
                 </div>
 
@@ -77,7 +121,7 @@ export default function MainGame() {
                 <div className="details">
                     <p id="user_choice"></p>
                     <p id="comp_choice"></p>
-                    <p id="result"></p>
+                    <p id="result">{result}</p>
                 </div>
             </div>
         </>
