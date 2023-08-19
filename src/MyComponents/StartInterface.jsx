5b67eea641ecc947884fd8c1b7aff905234ff448
JayeshSChaudhari <chaudhari.jayesh.cd.mec22@itbhu.ac.in>
2023-08-19 18:32:59 +0530

    requuired files created

---
+++ src/MyComponents/StartInterface.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../AllCSS/StartInterface.css'
+
+export default function StartInterface() {
+
+
+
+
+
+
+  return (
+    
+
+    <>
+
+
+    <button className="btnStart">Start</button>
+    <button className="btnStart">Exit</button>
+    
+    
+    
+    
+    
+    </>
+
+
+  )
+}
